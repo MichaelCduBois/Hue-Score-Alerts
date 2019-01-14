b3337b3718c987c5774ef0a669f2e96af3be7d2c
Michael duBois <mdubois@mcduboiswebservices.com>
2019-01-13 22:22:43 -0700

    Removed old comment.

--- HueScoreAlert/app_config.py
+++ HueScoreAlert/app_config.py
@@ -3,7 +3,6 @@ from flask import current_app, json
 
 def get():
 
-    # with open('app_config.json') as config_file:
     with current_app.open_resource('app_config.json') as config_file:
 
         configuration = json.load(config_file)
