edd8317c46030b848f091f0e9ab4bcc1cca1eb33
Michael duBois <mdubois@mcduboiswebservices.com>
2019-01-15 12:28:59 -0700

    Added get_state and trigger functions for use by scripts.

--- HueScoreAlert/hue.py
+++ HueScoreAlert/hue.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, json, redirect, render_template, url_for
 from HueScoreAlert import app_config
+from time import sleep
 import urllib.request as api
 
 # Creates Blueprint
@@ -15,11 +16,11 @@ def hello():
 
 def create_user():
 
-    configuration = app_config.get()
+    conf = app_config.get()
 
-    url = "http://" + configuration["bridge_ip"] + "/api"
+    url = "http://" + conf["bridge_ip"] + "/api"
 
-    json_data = {"devicetype": configuration["hue_device_name"]}
+    json_data = {"devicetype": conf["hue_device_name"]}
 
     params = json.dumps(json_data).encode('utf8')
 
@@ -55,3 +56,59 @@ def get_info(selection):
         json_list.append(return_json)
 
     return json_list
+
+
+def get_state(sport):
+
+    conf = app_config.get()
+
+    alert_type, alert_id = conf[sport + "_alert_selection_type"], conf[sport + "_alert_selection_id"]
+
+    url = "http://" + conf["bridge_ip"] + "/api/" + conf["user_token"] + "/" + alert_type + "/" + alert_id
+
+    response_json = json.loads(api.urlopen(url).read().decode())
+
+    hue_state = response_json["state"] if conf[sport + "_alert_selection_type"] == "lights" else response_json["action"]
+
+    current_state = {"on": hue_state["on"], "sat": hue_state["sat"], "bri": hue_state["bri"], "hue": hue_state["hue"]}
+
+    return current_state
+
+
+def trigger(sport):
+
+    original_state = get_state(sport)
+
+    conf = app_config.get()
+
+    # FixMe - This is PoC. Need to pull from app_config.json instead
+
+    ending = "state" if conf[sport + "_alert_selection_type"] == "lights" else "action"
+
+    url = "http://" + conf["bridge_ip"] + "/api/" + conf["user_token"] + "/" +\
+          conf[sport + "_alert_selection_type"] + "/" + conf[sport + "_alert_selection_id"] + "/" + ending
+
+    json_data = {"on": True, "sat": 254, "bri": 254, "hue": 4080, "alert": "select"}
+
+    params = json.dumps(json_data).encode("utf-8")
+
+    request = api.Request(url, data=params, headers=conf["headers"], method="PUT")
+
+    flash = 0
+
+    while flash != 3:
+
+        api.urlopen(request)
+
+        sleep(1.25)
+
+        flash += 1
+
+    # FixMe - This is end of PoC
+
+    # Returns to previous state
+    params = json.dumps(original_state).encode("utf-8")
+
+    request = api.Request(url, data=params, headers=conf["headers"], method="PUT")
+
+    api.urlopen(request)
