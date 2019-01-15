fac60d3cc902a2d998e0cd3979b9782edc55a873
Michael duBois <mdubois@mcduboiswebservices.com>
2019-01-14 22:24:19 -0700

    Expanded to include get_html and save_selections functions.

--- HueScoreAlert/nfl.py
+++ HueScoreAlert/nfl.py
@@ -1,16 +1,16 @@
-from flask import Blueprint, json, redirect, render_template, url_for
-from HueScoreAlert import app_config
-from urllib.request import urlopen as api_call
+from flask import Blueprint, json, Markup, redirect, render_template, request, url_for
+from HueScoreAlert import app_config, hue
+import urllib.request as api
 
 # Creates Blueprint
 bp = Blueprint('nfl', __name__, url_prefix='/nfl')
 
 
 # Initial Route Page
-@bp.route('/hello')
-def hello():
+@bp.route('/test')
+def test():
 
-    return 'Hello NFL World!'
+    return 'TEST: NFL'
 
 
 # Get NFL Teams
@@ -18,7 +18,7 @@ def get_teams():
 
     configuration = app_config.get()
 
-    response = api_call(configuration["nfl_url"] + "teams.json")
+    response = api.urlopen(configuration["nfl_url"] + "teams.json")
 
     team_json = json.loads(response.read().decode())
 
@@ -31,3 +31,33 @@ def get_teams():
         teams_list.append(json_team)
 
     return teams_list
+
+
+# NFL HTML Generation
+def get_html():
+
+    return Markup(render_template('packages/nfl.html',
+                                  nfl_teams=get_teams(),
+                                  lights=hue.get_info("lights"),
+                                  groups=hue.get_info("groups")))
+
+
+# Saves NFL Selections
+@bp.route('/save', methods=['POST'])
+def save_selections():
+
+    # NFL Team Selection
+    app_config.save("nfl_team_id", request.form["nfl_selection"].split(':')[0])
+    app_config.save("nfl_team_name", request.form["nfl_selection"].split(':')[1])
+
+    # NFL Alert Selection
+    app_config.save("nfl_alert_selection_type", request.form["lights_selection"].split(':')[0])
+    app_config.save("nfl_alert_selection_id", request.form["lights_selection"].split(':')[1])
+    app_config.save("nfl_alert_selection_name", request.form["lights_selection"].split(':')[2])
+
+    # TBD
+    app_config.save("nfl_alert_cycles", "none")
+    app_config.save("nfl_alert_color", "No Color Selected")
+    app_config.save("nfl_alert_style", "No Style Selected")
+
+    return redirect(url_for("dashboard.index"))
