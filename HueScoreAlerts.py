c4f8111cd371e0cd849a825c10d0af8c910f6bf6
MichaelCduBois <mdubois@mcduboiswebservices.com>
2019-03-01 12:26:08 -0700

    Added subprocess to start celery worker when starting app.

--- HueScoreAlerts.py
+++ HueScoreAlerts.py
@@ -1,6 +1,9 @@
 import HueScoreAlert
 import logging
+import subprocess
 from waitress import serve
+
+subprocess.Popen('celery -A HueScoreAlert.nhl worker'.split(), stdout=subprocess.PIPE)
 
 serve(HueScoreAlert.create_app(), listen='0.0.0.0:8080')
 
